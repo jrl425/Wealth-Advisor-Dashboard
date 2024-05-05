7bac34bd05c68dcd4b1e2a888df70a8385640a9f
DannyPress <155788626+DannyPress@users.noreply.github.com>
2024-05-05 17:47:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ returns_data = pd.read_csv('inputs/index_data.csv')
 st.write("Ticker Returns Data Loaded:")
 st.dataframe(returns_data.head())
 
-# Check and print the specific row and make sure it's all numeric
-st.write("Debug: Returns Data Row for Conversion:")
-st.write(returns_data.iloc[0, 1:])  # Adjust this to target the correct row/columns
-
-# Convert after confirming the data is as expected
-try:
-    returns = np.array(returns_data.iloc[0, 1:].astype(float))  # Convert to float
-except Exception as e:
-    st.error(f"Failed to convert returns data to float: {e}")
-
 # Load the covariance matrix data
 covariance_matrix = pd.read_csv('inputs/cov_mat.csv')
 st.write("\nCovariance Matrix Data Loaded:")
 st.dataframe(covariance_matrix.head())
+
+#####################################
+# Side bar code
+# Initialize the sidebar
+st.sidebar.header("User Inputs for Wealth Management")
+st.sidebar.subheader("Risk Aversion Survey")
+# Risk aversion input from 1 to 5
+risk_aversion = st.sidebar.slider("Select your risk aversion level:", 1, 5, 3)
+st.sidebar.subheader("Investment Details")
+# Input for investment amount
+investment_amount = st.sidebar.number_input("Enter the amount you want to invest:", min_value=1000, step=1000)
 
 ######################################
 # Utility function calculation
