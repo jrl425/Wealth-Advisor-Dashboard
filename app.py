c1aaae6cd5668f6cc8ba601de3dd1e2648042c1a
Jimmy <157163805+jrl425@users.noreply.github.com>
2024-05-06 16:47:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,9 @@ Our team, comprised of Jimmy, Ben, and Danny, brings a wealth of experience and 
 st.markdown(markdown_text_app)
 
 st.markdown("Meet Our Team")
-# Create three columns for the images
-col1, col2, col3 = st.beta_columns(3)
-
-# Display the first image in the first column
-with col1:
-    st.image("images/danny.jpg", caption="Image 1", use_column_width=True)
-
-# Display the second image in the second column
-with col2:
-    st.image("images/danny.jpg", caption="Image 2", use_column_width=True)
-
-# Display the third image in the third column
-with col3:
-    st.image("images/ben.jpg", caption="Image 3", use_column_width=True)
+st.image("images/jimmy.jpg", caption="Image 1", use_column_width=True)
+st.image("images/danny.jpg", caption="Image 2", use_column_width=True)
+st.image("images/ben.jpg", caption="Image 3", use_column_width=True)
 
 
 
