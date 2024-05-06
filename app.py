e90558c5f59cc37a3d9788616cbe7616f7548505
Jimmy <157163805+jrl425@users.noreply.github.com>
2024-05-06 16:04:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@
 
 import streamlit as st
 
+# Display the image from the images folder
+st.image("images/logo.png", caption="Your Logo Caption", use_column_width=True)
+
 
 # Set custom page title
 st.set_page_config(page_title="The Don Bowen Advisors Retirement Planning Dashboard")
