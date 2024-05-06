b972e43dcd505691055fd1126888f2366dae9d5b
Jimmy <157163805+jrl425@users.noreply.github.com>
2024-05-06 16:19:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("<p style='font-size:medium; color:red;'>Disclaimer: The content pro
 st.markdown("<hr style='border: 2px solid black;'>", unsafe_allow_html=True)
 st.markdown("Welcome to the Don Bowen Advisor’s Online Wealth Management Dashboard! This platform offers a straightforward approach to investment management, allowing users to input their financial preferences and goals via a simple sidebar interface. Our algorithm then generates a customized investment portfolio designed to match individual risk tolerances and objectives. Visualize your portfolio allocation and explore the securities comprising it. Gain insights into portfolio growth through simulations spanning from your current age to retirement. With a focus on user-friendly design and actionable insights, our dashboard provides a practical tool for investors of all levels to make informed decisions and optimize their financial strategies. Take charge of your financial future today with the Don Bowen Advisor’s Online Wealth Management Dashboard."
 
-
+ 
 # # Dashboard Title
 # st.title("The Don Bowen Advisors Retirement Planning Dashboard")
 # st.markdown("<p style='font-size:medium; color:red;'>Disclaimer: The content provided on this site is for educational purposes only and should not be considered as financial advice; users are encouraged to consult with a qualified financial advisor before making any investment decisions.</p>", unsafe_allow_html=True)
