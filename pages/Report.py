87b1e9838022a69da6c3db820e279f8c72e2a15c
Jimmy <157163805+jrl425@users.noreply.github.com>
2024-05-06 15:55:15 -0400

    Update Report.py

--- pages/Report.py
+++ pages/Report.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+from scipy.optimize import minimize 
+
 st.markdown("<p style='font-size:xx-Large; color:black;'>Report</p>", unsafe_allow_html=True)
