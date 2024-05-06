0779da4e6e1ba546b1109d9cd5e9a10e05eb903b
Jimmy <157163805+jrl425@users.noreply.github.com>
2024-05-06 16:46:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,15 +25,15 @@ col1, col2, col3 = st.beta_columns(3)
 
 # Display the first image in the first column
 with col1:
-    st.image("images/danny.jpg", caption="Image 1")
+    st.image("images/danny.jpg", caption="Image 1", use_column_width=True)
 
 # Display the second image in the second column
 with col2:
-    st.image("images/danny.jpg", caption="Image 2")
+    st.image("images/danny.jpg", caption="Image 2", use_column_width=True)
 
 # Display the third image in the third column
 with col3:
-    st.image("images/ben.jpg", caption="Image 3")
+    st.image("images/ben.jpg", caption="Image 3", use_column_width=True)
 
 
 
